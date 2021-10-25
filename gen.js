8acf43d84bd44cc6f2a9022bb18483c808f90701
Karoline <boothwhack@users.noreply.github.com>
2021-10-25 15:09:04 +0900

    Improve typescript definitions

--- gen.js
+++ gen.js
@@ -3,17 +3,19 @@ const parseOpenGl = require("./lib/ParseOpenGl");
 const generateSource = require("./lib/GenerateSource");
 
 (async () => {
-    if (process.argv.length < 4) throw new Error("Missing argument.");
+    if (process.argv.length < 5) throw new Error("Missing argument.");
 
-    const path = process.argv[2];
-    const outputPath = process.argv[3];
-    const content = await FS.readFile(path, {encoding: "utf-8"});
+    const [glXmlPath, cppOutputPath, dtsOutputPath] = process.argv.slice(2);
+
+    const content = await FS.readFile(glXmlPath, {encoding: "utf-8"});
 
     const objects = parseOpenGl(content);
-    const source = generateSource(objects);
+    const {cppSource, dtsSource} = generateSource(objects);
 
     debugger;
-    await FS.writeFile(outputPath, source.cppSource);
+
+    await FS.writeFile(cppOutputPath, cppSource);
+    await FS.writeFile(dtsOutputPath, dtsSource);
 })().catch(err => {
     console.error(err);
     process.exitCode = 1;
